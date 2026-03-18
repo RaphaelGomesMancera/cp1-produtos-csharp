963d9e1b79b8022602f8e19d506a4f1f55a73ae5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 20:04:21 -0300

    CP1 - Sistema de Controle de Produtos em C#

---
+++ Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Produto
+{
+    // Atributos privados
+    private string nome;
+    private double precoUnitario;
+    private int quantidade;
+    private double desconto;
+
+    // Construtor
+    public Produto(string nome, double precoUnitario, int quantidade, double desconto)
+    {
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+        this.desconto = desconto;
+    }
+
+    // Método para calcular custo total
+    public double CalcularCustoTotal()
+    {
+        return precoUnitario * quantidade;
+    }
+
+    // Método para calcular custo final (com desconto)
+    public double CalcularCustoFinal()
+    {
+        double total = CalcularCustoTotal();
+
+        if (total > 5000)
+        {
+            return total - (total * (desconto / 100));
+        }
+
+        return total;
+    }
+
+    // Método para exibir detalhes do produto
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine("\nProduto: " + nome);
+        Console.WriteLine("Custo Total: R$ " + CalcularCustoTotal());
+        Console.WriteLine("Custo Final: R$ " + CalcularCustoFinal());
+    }
+
+    // Getter para custo final (usado na comparação)
+    public double GetCustoFinal()
+    {
+        return CalcularCustoFinal();
+    }
+
+    // Getter para nome
+    public string GetNome()
+    {
+        return nome;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Produto 1
+        Console.WriteLine("=== Cadastro do Produto 1 ===");
+        Console.Write("Nome: ");
+        string nome1 = Console.ReadLine();
+
+        Console.Write("Preço unitário: ");
+        double preco1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Quantidade: ");
+        int qtd1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Desconto (%): ");
+        double desc1 = Convert.ToDouble(Console.ReadLine());
+
+        Produto p1 = new Produto(nome1, preco1, qtd1, desc1);
+
+        // Produto 2
+        Console.WriteLine("\n=== Cadastro do Produto 2 ===");
+        Console.Write("Nome: ");
+        string nome2 = Console.ReadLine();
+
+        Console.Write("Preço unitário: ");
+        double preco2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Quantidade: ");
+        int qtd2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Desconto (%): ");
+        double desc2 = Convert.ToDouble(Console.ReadLine());
+
+        Produto p2 = new Produto(nome2, preco2, qtd2, desc2);
+
+        // Exibir resultados
+        Console.WriteLine("\n=== RESULTADOS ===");
+        p1.ExibirDetalhes();
+        p2.ExibirDetalhes();
+
+        // Comparação
+        if (p1.GetCustoFinal() > p2.GetCustoFinal())
+        {
+            Console.WriteLine("\nProduto com maior custo final: " + p1.GetNome());
+        }
+        else if (p2.GetCustoFinal() > p1.GetCustoFinal())
+        {
+            Console.WriteLine("\nProduto com maior custo final: " + p2.GetNome());
+        }
+        else
+        {
+            Console.WriteLine("\nOs dois produtos possuem o mesmo custo final.");
+        }
+    }
+}
